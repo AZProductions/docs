d0d0b9495ea11251fb459c9b6f0a1d8f68b3492a
AZ Productions <66299945+AZProductions@users.noreply.github.com>
2022-04-17 13:24:45 +0200

    chore: experimental page loading

--- src/next.config.js
+++ src/next.config.js
@@ -2,16 +2,39 @@ const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
   options: {
     remarkPlugins: [],
-    rehypePlugins: [],
+    rehypePlugins: []
     // If you use `MDXProvider`, uncomment the following line.
     // providerImportSource: "@mdx-js/react",
-  },
-})
+  }
+});
 module.exports = withMDX({
+  basePath: '/docs'
   // Append the default value with md extensions
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-})
+  // pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx']
+});
 
 module.exports = {
-  basePath: '/docs',
-}
+  // Prefer loading of ES Modules over CommonJS
+  experimental: { esmExternals: true },
+  // Support MDX files as pages:
+  pageExtensions: ['md', 'mdx', 'tsx', 'ts', 'jsx', 'js'],
+  // Support loading `.md`, `.mdx`:
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.mdx?$/,
+      use: [
+        // The default `babel-loader` used by Next:
+        options.defaultLoaders.babel,
+        {
+          loader: '@mdx-js/loader',
+          /** @type {import('@mdx-js/loader').Options} */
+          options: {
+            /* jsxImportSource: …, otherOptions… */
+          }
+        }
+      ]
+    });
+
+    return config;
+  }
+};
